591775b7e0dd9120fc75cf2692f5640c6084ee2e
Samir <bsam@bk.ru>
2022-11-06 21:13:32 +0300

    add first not really functioning modal

--- src/routes/SharedDeck.tsx
+++ src/routes/SharedDeck.tsx
@@ -1,15 +1,52 @@
 import React, { useState } from 'react';
-import { Button, Collapse } from 'react-bootstrap';
+import { Button, Collapse, Modal, Stack } from 'react-bootstrap';
+import { decksActions } from '../store/decks';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
 import './SharedDeck.scss';
+import { ISharedDeck } from './SharedDecks';
 
 interface ISharedDeckProp {
-  description: string;
-  name: string;
-  phrase: Array<{ language: string; text: string }>;
+  deck: ISharedDeck;
 }
 
-const SharedDeck = ({ description, name, phrase }: ISharedDeckProp) => {
+const SharedDeck = ({ deck }: ISharedDeckProp) => {
+  const [showFixNameModal, setShowFixNameModal] = useState(false);
+  const { description, name } = deck;
+  const phrase = deck.phrases[0];
+  const deckNames = useAppSelector((state) =>
+    state.decks.decks.map((deck) => deck.name)
+  );
+  const dispatch = useAppDispatch();
   const [open, setOpen] = useState(false);
+  const handleSaveButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (deckNames.find((deckName) => deckName == deck.name)) {
+      setShowFixNameModal(true);
+      return;
+    }
+    dispatch(decksActions.saveSharedDeck({ deck: { ...deck, visible: true } }));
+  };
+
+  const translations = phrase.map((translation) => {
+    return (
+      <Stack direction="horizontal" className="shared-deck__translation">
+        <Stack className="shared-deck__stack-fix">
+          <p className="shared-deck__label">Language</p>
+          <p className="shared-deck__text">{translation.language}</p>
+        </Stack>
+        <Stack className="shared-deck__stack-fix">
+          <p className="shared-deck__label">Translation</p>
+          <p className="shared-deck__text">{translation.text}</p>
+        </Stack>
+      </Stack>
+    );
+  });
+
+  const handleCloseFixNameModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setShowFixNameModal(false);
+  };
+
   return (
     <>
       <Button
@@ -17,14 +54,30 @@ const SharedDeck = ({ description, name, phrase }: ISharedDeckProp) => {
         onClick={() => setOpen(!open)}
         aria-controls={name}
         aria-expanded={open}
-        variant="primary"
+        variant="info"
       >
         {name}
       </Button>
 
       <Collapse in={open}>
-        <p id={name}>{description}</p>
+        <Stack gap={2} id={name}>
+          <p className="shared-deck__description">{description}</p>
+          {translations}
+          <Button
+            variant="primary"
+            onClick={handleSaveButton}
+            className="shared-deck__save-button"
+          >
+            Save
+          </Button>
+        </Stack>
       </Collapse>
+
+      <Modal show={showFixNameModal} onHide={handleCloseFixNameModal}>
+        <Button onClick={handleCloseFixNameModal} variant="primary">
+          Close
+        </Button>
+      </Modal>
     </>
   );
 };
