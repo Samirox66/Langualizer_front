8db39bb376aa9996d6844ed1557907c071cc6b2f
Samir <bsam@bk.ru>
2022-11-08 15:38:51 +0300

    complete first modal

--- src/routes/SharedDeck.tsx
+++ src/routes/SharedDeck.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Collapse, Modal, Stack } from 'react-bootstrap';
+import { Button, Collapse, Form, Modal, Stack } from 'react-bootstrap';
 import { decksActions } from '../store/decks';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import './SharedDeck.scss';
@@ -10,21 +10,26 @@ interface ISharedDeckProp {
 }
 
 const SharedDeck = ({ deck }: ISharedDeckProp) => {
-  const [showFixNameModal, setShowFixNameModal] = useState(false);
-  const { description, name } = deck;
+  const [showFixName, setShowFixName] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [deckName, setDeckName] = useState(deck.name);
   const phrase = deck.phrases[0];
   const deckNames = useAppSelector((state) =>
     state.decks.decks.map((deck) => deck.name)
   );
+  let isDeckUnique = true;
+  if (deckNames.find((name) => deckName == name)) {
+    isDeckUnique = false;
+  }
+
   const dispatch = useAppDispatch();
   const [open, setOpen] = useState(false);
   const handleSaveButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (deckNames.find((deckName) => deckName == deck.name)) {
-      setShowFixNameModal(true);
-      return;
+    if (!isDeckUnique) {
+      setShowFixName(true);
     }
-    dispatch(decksActions.saveSharedDeck({ deck: { ...deck, visible: true } }));
+    setShowConfirmModal(true);
   };
 
   const translations = phrase.map((translation) => {
@@ -42,9 +47,20 @@ const SharedDeck = ({ deck }: ISharedDeckProp) => {
     );
   });
 
-  const handleCloseFixNameModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleCloseConfirmModal = () => {
+    setShowConfirmModal(false);
+    setShowFixName(false);
+  };
+
+  const handleConfirmButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setShowFixNameModal(false);
+    setShowConfirmModal(false);
+    setShowFixName(false);
+    dispatch(
+      decksActions.saveSharedDeck({
+        deck: { ...deck, visible: true, name: deckName },
+      })
+    );
   };
 
   return (
@@ -52,16 +68,16 @@ const SharedDeck = ({ deck }: ISharedDeckProp) => {
       <Button
         className="shared-deck__button"
         onClick={() => setOpen(!open)}
-        aria-controls={name}
+        aria-controls={deckName}
         aria-expanded={open}
         variant="info"
       >
-        {name}
+        {deck.name}
       </Button>
 
       <Collapse in={open}>
-        <Stack gap={2} id={name}>
-          <p className="shared-deck__description">{description}</p>
+        <Stack gap={2} id={deckName}>
+          <p className="shared-deck__description">{deck.description}</p>
           {translations}
           <Button
             variant="primary"
@@ -73,10 +89,39 @@ const SharedDeck = ({ deck }: ISharedDeckProp) => {
         </Stack>
       </Collapse>
 
-      <Modal show={showFixNameModal} onHide={handleCloseFixNameModal}>
-        <Button onClick={handleCloseFixNameModal} variant="primary">
-          Close
-        </Button>
+      <Modal show={showConfirmModal} onHide={handleCloseConfirmModal}>
+        <Modal.Body>
+          <Stack gap={2}>
+            {showFixName ? (
+              <>
+                <p>
+                  You already have a deck with this name, please, type unique
+                  name:
+                </p>
+                <Form.Control
+                  value={deckName}
+                  onChange={(e) => setDeckName(e.currentTarget.value)}
+                />
+              </>
+            ) : (
+              <>
+                <p>Do you want to save {deckName} deck?</p>
+              </>
+            )}
+            <Stack direction="horizontal" gap={3}>
+              <Button onClick={handleCloseConfirmModal} variant="primary">
+                Cancel
+              </Button>
+              <Button
+                onClick={handleConfirmButtonClick}
+                variant="primary"
+                disabled={!isDeckUnique}
+              >
+                Save
+              </Button>
+            </Stack>
+          </Stack>
+        </Modal.Body>
       </Modal>
     </>
   );
